47e6aa2cd8aec8999e4f87e11b493cb21fc47dc6
nguyendinhthongcdt <105415762+nguyendinhthongcdt@users.noreply.github.com>
2022-06-20 02:55:51 +0700

    Update web.py

--- web.py
+++ web.py
@@ -13,6 +13,22 @@ st.write("GVHD : PGS.TS Nguyễn Trường Thịnh")
 st.write("Họ và tên : Nguyễn Đình Thông")
 st.write("MSSV : 19146398")
 
+# This code is different for each deployed app.
+CURRENT_THEME = "blue"
+IS_DARK_THEME = True
+EXPANDER_TEXT = """
+    This is a custom theme. You can enable it by copying the following code
+    to `.streamlit/config.toml`:
+    ```python
+    [theme]
+    primaryColor = "#E694FF"
+    backgroundColor = "#00172B"
+    secondaryBackgroundColor = "#0083B8"
+    textColor = "#C6CDD4"
+    font = "sans-serif"
+    ```
+    """
+
 st.image('bg1.jpg')
 ### load file
 uploaded_file = st.file_uploader("Choose an image file", type=["jpg","jpeg","png"])
