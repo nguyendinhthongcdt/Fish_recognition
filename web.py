c37b6b2786b26b8788eeb86c21048a1e267d19d6
nguyendinhthongcdt <105415762+nguyendinhthongcdt@users.noreply.github.com>
2022-06-20 21:17:28 +0700

    Update web.py

--- web.py
+++ web.py
@@ -73,5 +73,7 @@ if uploaded_file is not None:
     if Genrate_pred:
         prediction = model.predict(img).argmax()
         st.write("**Kết quả nhận diện: {}**".format(map_dict [prediction])) 
+                    probabilityValue = np.amax(prediction) 
+                                st.write(round(probabilityValue*100, 2)) + " %"+"\n"
 st.warning("Lưu ý: Trang web này chỉ mang tính chất thao khảo")
 
