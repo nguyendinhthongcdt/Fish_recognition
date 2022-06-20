fb94ac502e2bf4b21e94eeb211a75f8517fb218e
nguyendinhthongcdt <105415762+nguyendinhthongcdt@users.noreply.github.com>
2022-06-20 13:35:55 +0700

    Update web.py

--- web.py
+++ web.py
@@ -9,8 +9,9 @@ from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_in
 
 # Tải model
 model = tf.keras.models.load_model("model.h5") 
-
-st.sidebar.text("Đây là sidebar")
+st.sidebar.[Information]
+with st.sidebar:
+            st.sidebar.text("Đây là sidebar")
 # Phần thông tin
 c1,c2=st.columns(2)
 with c2:
