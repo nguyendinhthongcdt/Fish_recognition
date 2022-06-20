46f12a7eb76db7222c2f380527a62874f0e0087d
nguyendinhthongcdt <105415762+nguyendinhthongcdt@users.noreply.github.com>
2022-06-20 09:35:24 +0700

    Update web.py

--- web.py
+++ web.py
@@ -16,38 +16,40 @@ st.header("MSSV : 19146398")
 
 st.title("Nhận diện các loại cá cảnh")
 st.image('bg1.jpg')
-# Upload file ảnh
+### load file
 uploaded_file = st.file_uploader("Upload hình cá cảnh", type=["jpg","jpeg","png","bmp"])
+
+map_dict = {0: 'Cá ba đuôi',
+            1: 'Cá cánh buồm hồng',
+            2:'Cá chép sư tử trắng',
+            3:'Cá hạc đỉnh hồng',
+            4:'Cá hổ bạc',
+            5:'Cá la hán',
+            6:'Cá mã giáp hoàng kim',
+            7:'Cá mún panda',
+            8:'Cá neon',
+            9:'Cá phượng hoàng',
+            10:'Cá rồng huyết long',
+            11:'Cá rồng kim long',
+            12:'Cá sam black diamond',
+            13:'Cá thần tiên',
+            14:'Cá tứ vân'}
+    
  
 if uploaded_file is not None:
-    # Chỉnh sửa ảnh đầu vào
+    # Convert the file
     img = image.load_img(uploaded_file,target_size=(64,64)) 
     st.image(uploaded_file, channels="RGB") 
     img = img_to_array(img)
     img = img.reshape(1,64,64,3)
     img = img.astype('float32')
     img = img/255
-# Gắn nhãn các lớp
-  map_dict = {0:'Cá ba đuôi',
-              1:'Cá cánh buồm hồng',
-              2:'Cá chép sư tử trắng',
-              3:'Cá hạc đỉnh hồng',
-              4:'Cá hổ bạc',
-              5:'Cá la hán',
-              6:'Cá mã giáp hoàng kim',
-              7:'Cá mún panda',
-              8:'Cá neon',
-              9:'Cá phượng hoàng',
-              10:'Cá rồng huyết long',
-              11:'Cá rồng kim long',
-              12:'Cá sam black diamond',
-              13:'Cá thần tiên',
-              14:'Cá tứ vân'}      
-            
-    # Nút nhấn
+        
+    #Button: nút dự đoán sau khi up ảnh
     Genrate_pred = st.button("Dự đoán") 
     
     if Genrate_pred:
+    
         prediction = model.predict(img).argmax()
         st.write("**Kết quả dự đoán {}**".format(map_dict [prediction])) 
 
